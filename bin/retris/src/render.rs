c73b63f543123b25a2636d4bccc1f0ee25039a8e
Peter Robinson <drinkcoffee@eml.cc>
2024-10-13 14:39:53 +1000

    Rotate and falling object

---
+++ bin/retris/src/render.rs
@@ -0,0 +1,81 @@
+use crate::state;
+
+use crossterm::{
+    cursor,
+    style::{self, Stylize},
+    terminal, ExecutableCommand, QueueableCommand,
+};
+use std::io::{self, Write};
+
+const BORDER: u16 = 1;
+const MAX_X: u16 = state::MAX_X + BORDER * 2;
+const MAX_Y: u16 = state::MAX_Y + BORDER * 2;
+
+pub fn render() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    // Draw border
+    for y in 0..MAX_Y {
+        for iter in 0..BORDER {
+            stdout
+                .queue(cursor::MoveTo(iter, y))?
+                .queue(style::PrintStyledContent("█".magenta()))?;
+            stdout
+                .queue(cursor::MoveTo(MAX_X - 1 - iter, y))?
+                .queue(style::PrintStyledContent("█".magenta()))?;
+        }
+    }
+    for x in 0..MAX_X {
+        for iter in 0..BORDER {
+            stdout
+                .queue(cursor::MoveTo(x, iter))?
+                .queue(style::PrintStyledContent("█".magenta()))?;
+            stdout
+                .queue(cursor::MoveTo(x, MAX_Y - 1 - iter))?
+                .queue(style::PrintStyledContent("█".magenta()))?;
+        }
+    }
+
+    // Draw screen
+    for y in 0..MAX_Y - 2 * BORDER {
+        for x in 0..MAX_X - 2 * BORDER {
+            let colour = state::get_locked(x, y);
+            if colour != 0 {
+                stdout
+                    .queue(cursor::MoveTo(x + BORDER, y + BORDER))?
+                    .queue(style::PrintStyledContent("X".red()))?;
+            }
+        }
+    }
+
+    let coords = state::get_falling_coords();
+    let x1 :u16 = coords.0;
+    let y1 :u16 = coords.1;
+    let x2 :u16 = coords.2;
+    let y2 :u16 = coords.3;
+    let x3 :u16 = coords.4;
+    let y3 :u16 = coords.5;
+    let x4 :u16 = coords.6;
+    let y4 :u16 = coords.7;
+
+    draw_dot(x1, y1).unwrap();
+    draw_dot(x2, y2).unwrap();
+    draw_dot(x3, y3).unwrap();
+    draw_dot(x4, y4).unwrap();
+
+    stdout.flush()?;
+    Ok(())
+}
+
+fn draw_dot(x: u16, y: u16)  -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    if state::check_valid(x, y) {
+        stdout
+        .queue(cursor::MoveTo(x,y))?
+        .queue(style::PrintStyledContent( "X".blue()))?;
+    }
+    Ok(())
+}
--- bin/retris/src/state.rs
+++ bin/retris/src/state.rs
@@ -1,19 +1,153 @@
 use std::panic;
 
+pub const MAX_X: u16 = 20;
+pub const MAX_Y: u16 = 40;
 
-const MAX_X: usize = 20;
-const MAX_Y: usize = 40;
+#[derive(Clone, Copy)]
+enum FourObjType {
+    None,
+    Straight,
+    Square,
+    LeftTrap,
+    RightTrap,
+    LShape,
+    ReverseLShape,
+}
 
-static mut STATE: [[u8; MAX_X]; MAX_Y] = [[0u8; MAX_X]; MAX_Y];
+static mut STATE: [[u8; MAX_Y as usize]; MAX_X as usize] = [[0u8; MAX_Y as usize]; MAX_X as usize];
+
+static mut FALLING_TYPE: FourObjType = FourObjType::None;
+static mut FALLING_LOCATION_Y: i16 = -2;
+static mut FALLING_LOCATION_X: u16 = MAX_X / 2;
+static mut FALLING_ORIENTATION: u16 = 0;
+
+pub fn start_new_four() {
+    unsafe {
+        FALLING_TYPE = FourObjType::Straight;
+        FALLING_LOCATION_Y = -2;
+        FALLING_LOCATION_X = MAX_X / 2;
+    }
+}
+
+pub fn fall_one() {
+    unsafe {
+        FALLING_LOCATION_Y += 1;
+    }
+}
+
+pub fn move_left() {
+    unsafe {
+        if FALLING_LOCATION_X > 0 {
+            FALLING_LOCATION_X -= 1;
+        }
+    }
+}
+
+pub fn move_right() {
+    unsafe {
+        if FALLING_LOCATION_X < MAX_X - 1 {
+            FALLING_LOCATION_X += 1;
+        }
+    }
+}
+
+pub fn rotate_clockwise() {
+    unsafe {
+        FALLING_ORIENTATION += 1;
+        if FALLING_ORIENTATION == 4 {
+            FALLING_ORIENTATION = 0;
+        }
+    }
+}
+
+pub fn rotate_anticlockwise() {
+    unsafe {
+        if FALLING_ORIENTATION == 0 {
+            FALLING_ORIENTATION = 4;
+        }
+        FALLING_ORIENTATION -= 1;
+    }
+}
 
 
-pub fn get_colour(x: usize, y: usize) -> u8 {
+
+pub fn get_locked(x: u16, y: u16) -> u8 {
     if x >= MAX_X {
         panic!("X too big: {}", x)
     }
     if y >= MAX_Y {
         panic!("Y too big: {}", x)
     }
+    //println!("X {} Y {}", x,y);
 
-    unsafe{STATE[x][y]}
+    let colour = unsafe { STATE[x as usize][y as usize] };
+    // if colour == 0 {
+
+    //     // unsafe {
+    //     //     if FALLING_LOCATION_X == x && FALLING_LOCATION_Y == y as i16 {
+    //     //         colour = 1;
+    //     //     }
+    //     // }
+    // }
+    return colour;
+}
+
+pub fn get_falling_coords() -> (u16, u16, u16, u16, u16, u16, u16, u16) {
+    let mut x1: u16 = 0;
+    let mut x2: u16 = 0;
+    let mut x3: u16 = 0;
+    let mut x4: u16 = 0;
+    let mut y1: u16 = 0;
+    let mut y2: u16 = 0;
+    let mut y3: u16 = 0;
+    let mut y4: u16 = 0;
+
+    let fall_type;
+    let fall_x;
+    let fall_y;
+    let fall_orientation;
+
+    unsafe {
+        fall_type = FALLING_TYPE;
+        fall_x = FALLING_LOCATION_X;
+        fall_y = FALLING_LOCATION_Y;
+        fall_orientation = FALLING_ORIENTATION;
+    }
+
+    match fall_type {
+        FourObjType::None =>{ panic!("Fall type not set"); },
+        FourObjType::Straight=> {
+            match fall_orientation {
+                0 | 2 => {
+                    x1 = fall_x;
+                    x2 = fall_x + 1;
+                    x3 = fall_x + 2;
+                    x4 = fall_x + 3;
+                    y1 = fall_y as u16;
+                    y2 = fall_y as u16;
+                    y3 = fall_y as u16;
+                    y4 = fall_y as u16;
+                },
+                1 | 3 => {
+                    x1 = fall_x;
+                    x2 = fall_x;
+                    x3 = fall_x;
+                    x4 = fall_x;
+                    y1 = fall_y as u16;
+                    y2 = fall_y as u16+ 1;
+                    y3 = fall_y as u16 + 2;
+                    y4 = fall_y as u16 + 3;
+                },
+                _ => { panic!("Unknown orientation");}
+            }
+        },
+        _=>{panic!("Not supported yet"); }
+    }
+
+    return (x1, y1, x2, y2, x3, y3, x4, y4);
+
+}
+
+pub fn check_valid(x: u16, y: u16) -> bool {
+    return x < MAX_X && y < MAX_Y;
 }
